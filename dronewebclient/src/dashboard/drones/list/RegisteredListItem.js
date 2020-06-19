dee7d859b71bce1fcfc56e413b32636e41dc689b
Artsiom Kaliaha <stakkato95@gmail.com>
2020-06-19 22:07:24 +0200

    Update grid structure for drones list

--- dronewebclient/src/dashboard/drones/list/RegisteredListItem.js
+++ dronewebclient/src/dashboard/drones/list/RegisteredListItem.js
@@ -47,46 +47,52 @@ function RegisteredListItem(props) {
             <TableRow>
                 <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
                     <Collapse in={expanded} timeout="auto" unmountOnExit>
-                        <Box margin={1} display='flex'>
-                            {/* <Typography variant="h6" gutterBottom component="div">
-                                Additional information
-                            </Typography> */}
-                            <img src="https://image.flaticon.com/icons/svg/215/215767.svg" width='250px' height='250px'/>
-                            <Table className={classes.table} size="small">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Parameter</TableCell>
-                                        <TableCell align="right">Value</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    <TableRow key="id">
-                                        <TableCell component="th" scope="row">Id</TableCell>
-                                        <TableCell align="right">{row.id}</TableCell>
-                                    </TableRow>
-                                    <TableRow key="ip">
-                                        <TableCell component="th" scope="row">IP</TableCell>
-                                        <TableCell align="right">{row.ip}</TableCell>
-                                    </TableRow>
-                                    <TableRow key="lat">
-                                        <TableCell component="th" scope="row">Show up time</TableCell>
-                                        <TableCell align="right">{row.showUpTime}</TableCell>
-                                    </TableRow>
-                                    <TableRow key="lon">
-                                        <TableCell component="th" scope="row">Registration time</TableCell>
-                                        <TableCell align="right">{row.registrationTime}</TableCell>
-                                    </TableRow>
-                                    <TableRow key="alt">
-                                        <TableCell component="th" scope="row">Last connection time</TableCell>
-                                        <TableCell align="right">{row.lastConnectionTime}</TableCell>
-                                    </TableRow>
-                                    <TableRow key="alt">
-                                        <TableCell component="th" scope="row">Last seen time</TableCell>
-                                        <TableCell align="right">{row.lastSeenTime}</TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-                        </Box>
+                        <Grid container spacing={1}>
+                            <Grid item xs={12}>
+                                <Typography variant="h6" gutterBottom component="div">
+                                    Additional information
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <img src="https://image.flaticon.com/icons/svg/215/215767.svg" />
+                            </Grid>
+                            <Grid item xs={9}>
+                                <Table className={classes.table} size="small">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>Parameter</TableCell>
+                                            <TableCell align="right">Value</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        <TableRow key="id">
+                                            <TableCell component="th" scope="row">Id</TableCell>
+                                            <TableCell align="right">{row.id}</TableCell>
+                                        </TableRow>
+                                        <TableRow key="ip">
+                                            <TableCell component="th" scope="row">IP</TableCell>
+                                            <TableCell align="right">{row.ip}</TableCell>
+                                        </TableRow>
+                                        <TableRow key="lat">
+                                            <TableCell component="th" scope="row">Show up time</TableCell>
+                                            <TableCell align="right">{row.showUpTime}</TableCell>
+                                        </TableRow>
+                                        <TableRow key="lon">
+                                            <TableCell component="th" scope="row">Registration time</TableCell>
+                                            <TableCell align="right">{row.registrationTime}</TableCell>
+                                        </TableRow>
+                                        <TableRow key="alt">
+                                            <TableCell component="th" scope="row">Last connection time</TableCell>
+                                            <TableCell align="right">{row.lastConnectionTime}</TableCell>
+                                        </TableRow>
+                                        <TableRow key="alt">
+                                            <TableCell component="th" scope="row">Last seen time</TableCell>
+                                            <TableCell align="right">{row.lastSeenTime}</TableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                </Table>
+                            </Grid>
+                        </Grid>
                     </Collapse>
                 </TableCell>
             </TableRow>
