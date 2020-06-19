57f114a7fccb64932770a205ab09ccf9069455e2
Artsiom Kaliaha <stakkato95@gmail.com>
2020-06-19 12:33:30 +0200

    Update dialog content

--- dronewebclient/src/dashboard/drones/RegisterDroneDialog.js
+++ dronewebclient/src/dashboard/drones/RegisterDroneDialog.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+
+import { Button, TextField, Switch, FormControlLabel } from '@material-ui/core';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
 
 class RegisterDroneDialog extends React.Component {
 
@@ -24,10 +20,13 @@ class RegisterDroneDialog extends React.Component {
                     <DialogTitle id="form-dialog-title">Register a new drone</DialogTitle>
                     <DialogContent>
                         <DialogContentText>
-                            To subscribe to this website, please enter your email address here. We will send updates
-                            occasionally.
+                            To create a new drone, please enter its name. You can also automatically start a session with this drone.
                         </DialogContentText>
-                        <TextField autoFocus margin="dense" id="name" label="Email Address" type="text" fullWidth />
+                        <TextField autoFocus margin="dense" id="name" label="Drone name" type="text" fullWidth />
+                        <FormControlLabel
+                            control={<Switch />}
+                            label="Automatically start session"
+                        />
                     </DialogContent>
                     <DialogActions>
                         <Button onClick={handleCancel} color="primary">Cancel</Button>
