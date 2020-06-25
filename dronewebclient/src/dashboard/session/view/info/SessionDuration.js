dba429ed1643d3964509c43e6dad34b068ab6ea0
Artsiom Kaliaha <stakkato95@gmail.com>
2020-06-25 08:38:47 +0200

    Add logic for stopping timer at session screen, when session is interrupted

--- dronewebclient/src/dashboard/session/view/info/SessionDuration.js
+++ dronewebclient/src/dashboard/session/view/info/SessionDuration.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import { Typography } from '@material-ui/core';
 
 import { getTimeDifference } from '../../../../helper/CommonHelper';
+import { SessionState } from '../../../../model/SessionState';
 
 function SessionDuration() {
     const session = useSelector(state => state.session.session);
@@ -11,7 +12,11 @@ function SessionDuration() {
     const [elapsedTime, setElapsedTime] = useState(getTimeDifference());
 
     useEffect(() => {
-        setTimeout(() => setElapsedTime(getTimeDifference(session.sessionStartTime)), 1000);
+        setTimeout(() => {
+            if (session.sessionState === SessionState.RUNNING) {
+                setElapsedTime(getTimeDifference(session.sessionStartTime))
+            }
+        }, 1000);
     });
 
     return (<>
