658298cae0c650c0ec264f095e58968e21573049
Artsiom Kaliaha <stakkato95@gmail.com>
2020-06-27 12:13:07 +0200

    Revert "Add charts js streaming lib for real time data"
    This reverts commit d97c02db7b08501d403a5f6e09a502ea28c4e45d.

--- dronewebclient/src/dashboard/session/telemetry/view/Telemetry.js
+++ dronewebclient/src/dashboard/session/telemetry/view/Telemetry.js
@@ -14,10 +14,6 @@ import { Close } from '@material-ui/icons';
 
 import { listenGnssSse, listenRotationSse, listenSpeedSse } from '../architecture/redux/TelemetryActions';
 
-
-import { Line } from 'react-chartjs-2';
-import 'chartjs-plugin-streaming';
-
 const useStyles = makeStyles((theme) => ({
     paper: {
         padding: '16px'
@@ -35,45 +31,9 @@ function Telemetry(props) {
 
     const classes = useStyles();
 
-    const onRefresh = (chart) => {
-        chart.data.datasets.forEach(function (dataset) {
-            dataset.data.push({
-                x: Date.now(),
-                y: Math.random()
-            });
-        });
-    };
-
     return (<div>
-        <Line
-            data={{ datasets: [{ data: [] }] }}
-            options={{
-                scales: {
-                    xAxes: [{
-                        type: 'realtime',
-                        realtime: {
-                            duration: 20000,
-                            refresh: 1000,
-                            delay: 2000,
-                            pause: false,
-                            ttl: undefined,
-                            onRefresh: onRefresh
-                        }
-                    }]
-                }
-            }}
-        />
         {gnss.length}
     </div>);
 }
 
 export default Telemetry;
-
-const onRefresh = (chart) => {
-    chart.data.datasets.forEach(function (dataset) {
-        dataset.data.push({
-            x: Date.now(),
-            y: Math.random()
-        });
-    });
-};
